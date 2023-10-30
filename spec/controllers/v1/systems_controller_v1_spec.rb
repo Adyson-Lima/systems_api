d4b8231277e7fab4448ced82ffb80eebcfe02e2f
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-10-30 10:38:20 -0300

    13° commit - tdd controller: Consegue listar um system especifico e retornar status 200? RED

--- spec/controllers/v1/systems_controller_v1_spec.rb
+++ spec/controllers/v1/systems_controller_v1_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Api::V1::SystemsController, type: :controller do
     end
   end
 
+  describe 'GET /api/v1/systems/id' do
+    it 'Consegue listar um system especifico e retornar status 200?' do
+      get :show, params: {id: @system.id}
+      expect(response.body).to include_json(id: @system.id)
+      expect(response).to have_http_status(200)
+    end
+  end
+
 end
