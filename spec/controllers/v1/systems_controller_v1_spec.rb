d543bd83a3d377df7f8c9ccb36c1d6390f4a50d8
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-10-30 10:51:11 -0300

    15° commit tdd controller: Consegue criar um system e retornar status 201? RED

--- spec/controllers/v1/systems_controller_v1_spec.rb
+++ spec/controllers/v1/systems_controller_v1_spec.rb
@@ -20,4 +20,13 @@ RSpec.describe Api::V1::SystemsController, type: :controller do
     end
   end
 
+  describe 'POST /api/v1/systems' do
+    it 'Consegue criar um system e retornar status 201?' do
+      post :create, params: {system: {name: "debian", manufacturer: "community"}, format: :json}
+
+      expect(response.body).to include_json(name: "debian")
+      expect(response).to have_http_status(201)
+    end
+  end
+
 end
