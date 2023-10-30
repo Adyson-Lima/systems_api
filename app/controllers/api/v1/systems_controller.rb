27b1e06da0ad2d5442a67c440c7c9b4ad9d216de
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-10-30 10:40:28 -0300

    14° commit - tdd controller: Consegue listar um system especifico e retornar status 200? GREEN

--- app/controllers/api/v1/systems_controller.rb
+++ app/controllers/api/v1/systems_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::SystemsController < ApplicationController
 
-  #before_action :set_system, only: %i[] #show update destroy
+  before_action :set_system, only: %i[ show ] #show update destroy
 
   def index
     @systems = System.all
     render json: @systems
+  end
+
+  def show
+    render json: @system
   end
 
 private
